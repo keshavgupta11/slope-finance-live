9e79c010dac822503420f952d93135556a67a902
Keshav Gupta <keshavgupta@Keshavs-MacBook-Air.local>
2025-07-08 15:53:10 -0400

    fix fee strucutre

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ const getProvider = () => {
 export default function App() {
   const initialMarketSettings = {
     "JitoSol": { apy: 7.9, k: 0.00001, symbol: "JitoSOL" }, // Based on your 2025 projection
-    "Lido stETH": { apy: 3.0, k: 0.00002, symbol: "stETH" },
+    "Lido stETH": { apy: 3.0, k: 0.000005, symbol: "stETH" },
     "Ethena sUSDe": { apy: 4.0, k: 0.00001, symbol: "sUSDe" },
   };
 
