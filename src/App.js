d489134c30cc6942eb43af3079eed79cba019703
Keshav Gupta <keshavgupta@Keshavs-MacBook-Air.local>
2025-07-16 17:00:10 -0400

    fix fee strucutre

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useMemo } from 'react';
+import React, { useState, useEffect, useMemo, useRef } from 'react';
 import { LineChart, Line, XAxis, YAxis, ResponsiveContainer } from 'recharts';
 import './App.css';
 
