c87640e9ccd6ffca657e06c72a9a64fd118b7d84
Keshav Gupta <keshavgupta@Mac-68.lan>
2025-07-18 15:30:40 -0400

    fix fee strucutre

--- src/App.js
+++ src/App.js
@@ -4633,20 +4633,16 @@ const calculateVammBreakdown = () => {
                   <span>Margin:</span>
                   <span>${margin.toLocaleString()}</span>
                 </div>
+                <div style={{ 
+                  textAlign: 'center', 
+                  fontSize: '0.8rem', 
+                  color: '#10b981',
+                  fontStyle: 'italic',
+                  marginTop: '0.5rem'
+                }}>
+                  Trading 2025 full-year APY (compounded)
+                </div>
               </div>
-            </div>
-            <div style={{
-              textAlign: 'center',
-              padding: '0.6rem',
-              fontSize: '0.8rem',
-              color: '#10b981',
-              fontWeight: '600',
-              fontStyle: 'italic',
-              borderTop: '1px solid rgba(55, 65, 81, 0.2)',
-              marginTop: '1rem',
-              paddingTop: '1rem'
-            }}>
-              Trading 2025 full-year APY (compounded)
             </div>
             {/* DV01 Impact Explanation */}
             <div style={{
