e1523b56138e802cffabbc2fefb89b9fb2b54ffe
Keshav Gupta <keshavgupta@Mac-68.lan>
2025-07-18 15:53:44 -0400

    fix fee strucutre

--- src/App.js
+++ src/App.js
@@ -1903,7 +1903,7 @@ const calculateVammBreakdown = () => {
               }}>
                 Crypto Rate Swaps (CRS)<span style={{ marginLeft: '1rem' }}></span>1 Jan-31 Dec'25
               </div>
-              <div className="price-info">
+              <div className="price-info" style={{ marginBottom: '0.5rem' }}>
                 <div className="price-row">
                   <span>Live Price:</span>
                   <span style={{ color: '#ffffff', fontWeight: '700', fontSize: '1rem', position: 'relative' }}>
